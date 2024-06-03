371f9af7e49e1d3a3339be35af03fff66e208aab
HlyGravies <nguyenhoang887984@gmail.com>
2024-06-03 16:37:06 +0900

    ok

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, render } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createVuetify } from 'vuetify'
 
 createApp(App).mount('#app')
+
+// Tạo ứng dụng Vue
+const app = createApp(App)
+
+// Sử dụng Vuetify
+app.use(createVuetify())
+
+// Mount ứng dụng với Vuetify vào một phần tử với id là 'app'
+app.mount('#app')
